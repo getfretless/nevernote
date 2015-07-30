c988c47022e7668019f4f0587633e763c6a0aa77
Dave Strus <dave@davestrus.com>
2015-07-30 15:43:03 -0400

    Add session support to API.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :notes, except: [:new, :edit]
+      resource :session, only: :create
     end
   end
 end
